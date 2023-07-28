730a45c933cd3e1014e4900bc0ac731a57095d1e
LucyL-J <65664690+LucyL-J@users.noreply.github.com>
2023-07-28 16:40:27 +0100

    Simulations to test validity of approximations

--- population_dynamics.jl
+++ population_dynamics.jl
@@ -5,11 +5,11 @@ using Roots, Distributions
 pop_size(t, N0, pop_growth) = N0*exp(pop_growth*t)
 # Growth of response-on subpopulation: influx from response-off subpopulation plus exponential growth with own net growth rate = division-death rate, initial population size N0_on 
 # Initial population size is set to zero
-function pop_size(t, N0, pop_growth, switching, net_growth_on, N0_on)
+function pop_size(t, N0_off, pop_growth, switching, N0_on, net_growth_on)
     if net_growth_on == pop_growth
-        return N0*switching*t*exp(pop_growth*t)
+        return N0_off*switching*t*exp(pop_growth*t)
     else
-        return N0*switching/(pop_growth - net_growth_on) * (exp(pop_growth*t) - exp(net_growth_on*t)) + N0_on*exp(net_growth_on*t)
+        return N0_off*switching/(pop_growth - net_growth_on) * (exp(pop_growth*t) - exp(net_growth_on*t)) + N0_on*exp(net_growth_on*t)
     end
 end
 
@@ -17,20 +17,25 @@ end
 # Time until next event (either switching or mutation) for response-off subpopulation
 interval_event(t, N0, pop_growth, event_rate, y) = log(y*pop_growth/(event_rate*pop_size(t, N0, pop_growth)) + 1) / pop_growth
 # Time until next mutation event for response-on subpopulation
-function interval_event(t, N0, pop_growth, switching, net_growth_on, N0_on, T, event_rate, y)
+function interval_event(t, N0_off, pop_growth, switching, N0_on, net_growth_on, T, event_rate, y)
     if net_growth_on == 0.
-        return interval_event(t, switching*N0/pop_growth, pop_growth, event_rate, y)
-    elseif net_growth_on == pop_growth
-        f1(tau) = event_rate/pop_growth * ((N0*switching*tau+N0_on) * exp(pop_growth*(t+tau)) + N0*switching*(t-1/pop_growth)/pop_growth * (exp(pop_growth*tau) - 1) - N0_on*exp(pop_growth*tau)) - y
+        f1(tau) = event_rate * (N0_off*switching/pop_growth * (exp(pop_growth*(t + tau))/pop_growth - exp(pop_growth*t)/pop_growth) - (N0_off*switching/pop_growth - N0_on) * tau) - y
         try
             return find_zero(f1, (0., T))
         catch e
             return T
         end
-    else
-        f2(tau) = event_rate * (N0*switching/(pop_growth-net_growth_on) * (exp(pop_growth*(t + tau))/pop_growth - exp(pop_growth*t)/pop_growth) - (N0*switching/(pop_growth-net_growth_on) - N0_on) * (exp(net_growth_on*(t + tau))/net_growth_on - exp(net_growth_on*t)/net_growth_on)) - y
+    elseif net_growth_on == pop_growth
+        f2(tau) = event_rate/pop_growth * ((N0_off*switching*tau+N0_on) * exp(pop_growth*(t+tau)) + N0_off*switching*(t-1/pop_growth)/pop_growth * (exp(pop_growth*tau) - 1) - N0_on*exp(pop_growth*tau)) - y
         try
             return find_zero(f2, (0., T))
+        catch e
+            return T
+        end
+    else
+        f3(tau) = event_rate * (N0_off*switching/(pop_growth-net_growth_on) * (exp(pop_growth*(t + tau))/pop_growth - exp(pop_growth*t)/pop_growth) - (N0_off*switching/(pop_growth-net_growth_on) - N0_on) * (exp(net_growth_on*(t + tau))/net_growth_on - exp(net_growth_on*t)/net_growth_on)) - y
+        try
+            return find_zero(f3, (0., T))
         catch e
             return T
         end
@@ -93,4 +98,47 @@ function number_cells(T, birth_off, death_off, switching, birth_on, death_on)
         end
         return [m_off, m_on]
     end
+end
+
+# Testing validity of approximations
+# Simulating non-mutant dynamics: response-off subpopulation deterministic and response-on subpopulation stochastic
+# Returns the number of response-on cells at time T
+function non_mutants_on(T, N0_off, pop_growth, switching, N0_on::Int, division_on, death_on)
+    t = 0.
+    n_on = 0
+    for i = 1:N0_on
+        n_on += number_cells(T, division_on, death_on)
+    end
+    while t < T
+        y = rand(Exponential(1))
+        tau = interval_event(t, N0_off, pop_growth, switching, y)
+        t += tau
+        n_on += number_cells(T-t, division_on, death_on)
+    end
+    return n_on
+end
+# Simulating mutant dynamics; non-mutants deterministic and mutants (with switching) stochastic 
+# Returns the number of mutants which 
+# (i) come from the response-off subpopulation and did not switch from response-off to response-on
+# (ii) come from the response-off subpopulation and switched from response-off to response-on
+# (iii) come from the response-on subpopulation
+function mutants_off_switch_on(T, N0_off, division_off, death_off, mutation_off, switching, N0_on, division_on, death_on, mutation_on)
+    pop_growth = division_off - death_off - switching
+    t = 0.
+    m_off_switch = [0, 0]
+    while t < T
+        y = rand(Exponential(1))
+        tau = interval_event(t, N0_off, pop_growth, mutation_off, y)
+        t += tau
+        m_off_switch .+= number_cells(T-t, division_off, death_off, switching, division_on, death_on)
+    end
+    t = 0.
+    m_on = 0
+    while t < T
+        y = rand(Exponential(1))
+        tau = interval_event(t, N0_off, pop_growth, switching, N0_on, division_on-death_on, T, mutation_on, y)
+        t += tau
+        m_on += number_cells(T-t, division_on, death_on)
+    end
+    return [m_off_switch; m_on]
 end
