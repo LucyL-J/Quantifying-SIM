12e1135b248641c08896f82de6dd7afc75e7a09d
LucyL-J <65664690+LucyL-J@users.noreply.github.com>
2023-07-28 14:59:32 +0100

    Initial population size response-on

--- population_dynamics.jl
+++ population_dynamics.jl
@@ -3,7 +3,7 @@
 pop_size(t, N0, pop_growth) = N0*exp(pop_growth*t)
 # Growth of response-on subpopulation: influx from response-off subpopulation plus exponential growth with own net growth rate = division-death rate, initial population size N0_on 
 # Initial population size is set to zero
-function pop_size(t, N0, pop_growth, switching, net_growth_on)
+function pop_size(t, N0, pop_growth, switching, net_growth_on, N0_on)
     if net_growth_on == pop_growth
         return N0*switching*t*exp(pop_growth*t)
     else
