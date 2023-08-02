5ec4230de2a7d6ee5f67e53607f60c6c4dc8f27c
LucyL-J <65664690+LucyL-J@users.noreply.github.com>
2023-08-02 11:36:37 +0100

    Mutant count distribution for a heterogeneous population.

--- inference.jl
+++ inference.jl
@@ -4,7 +4,7 @@ using Distributions, SpecialFunctions
 # Keller, P., & Antal, T. (2015). Mutant number distribution in an exponentially growing population. Journal of Statistical Mechanics: Theory and Experiment, 2015(1), P01011. https://doi.org/10.1088/1742-5468/2015/01/P01011
 
 # Muntant count distribution for a homogeneous population with optional differential fitness of mutants
-function P_mutant_count(K::Int, mutation_per_gen, Nf; fitness_m=1.)    # Mutations generation = mutation rate [1/h] / growth rate [1/h]
+function P_mutant_count(K::Int, mutation_per_gen, Nf; fitness_m=1.)    # Mutations per generation = mutation rate [1/h] / growth rate [1/h]
     K_bound = minimum([K, 1000])                                       # Introducing an arbritraty cut-off at 1000 colonies; any mutant count >1000 is considered as =1000 instead
     p = zeros(Float64, K_bound+1)
     if fitness_m == 0.
@@ -39,4 +39,21 @@ function Q(K::Int, mutation_per_gen, fitness_m, Nf)
         end
     end
     return q
+end
+
+# Mutant count distribution for a heterogeneous population with response-off and -on subpopulation. The relative division rate of on cells is an optional input parameter with default value of zero
+# Mutation rates (for both response-off and -on cells) are given in mutations per division of response-off cells and scaled to be in units of mutations per generation
+function P_mutant_count(K::Int, mutation_off_per_div, Nf, mutation_on_per_div, f_on; rel_division_on=0.)
+    p_off = P_mutant_count(K, mutation_off_per_div/(1-f_on*(1-rel_division_on)), (1-f_on)*Nf)                                   
+    p_on = P_mutant_count(K, mutation_on_per_div/(1-f_on*(1-rel_division_on)), f_on*Nf, fitness_m=rel_division_on/(1-f_on*(1-rel_division_on))) 
+    K_bound = length(p_off)-1
+    p = zeros(Float64, K_bound+1)
+    for k = 0:K_bound
+        pk = 0
+        for j = 0:k
+            pk += p_off[j+1] * p_on[k-j+1] # pdf of the total mutant count (response-off + -on)
+        end
+        p[k+1] = pk
+    end
+    return p
 end
